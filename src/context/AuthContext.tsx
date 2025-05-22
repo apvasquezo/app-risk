2887d617cea84ce561b6ba3dabf08ba6a7855265
apvasquezo <nanitave@gmail.com>
2025-05-22 08:47:11 -0500

    modify authentication

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import React, { createContext, useContext, useState, useEffect, ReactNode } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
 
 // Define la estructura del usuario
 interface User {
@@ -11,9 +17,10 @@ interface User {
 interface AuthContextType {
   user: User | null;
   setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  loading: boolean;
 }
 
-// Crea el contexto con un valor predeterminado como `undefined`
+// Crea el contexto
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 // Define el tipo de las props del AuthProvider
@@ -23,22 +30,32 @@ interface AuthProviderProps {
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
-      const payload = JSON.parse(atob(token.split(".")[1])); // Decodifica el token
-      setUser({ role: payload.role }); // Establece el usuario con el rol
+      try {
+        const payload = JSON.parse(atob(token.split(".")[1])); // Decodifica el payload
+        if (payload && payload.role) {
+          setUser({ role: payload.role });
+        }
+      } catch (error) {
+        console.error("Error al decodificar el token:", error);
+        setUser(null); // En caso de error, se borra el usuario
+      }
     }
+    setLoading(false); // Ya sea con o sin token, se termina de cargar
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, loading }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
+// Hook personalizado para consumir el contexto
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
@@ -46,3 +63,4 @@ export const useAuth = () => {
   }
   return context;
 };
+
